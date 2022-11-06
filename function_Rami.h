c515e80f5ae6812c4a5dedb46ac4fe17eb188735
ramilaribi <116126157+ramilaribi@users.noreply.github.com>
2022-11-06 08:28:53 -1100

    Update function_Rami.h

--- function_Rami.h
+++ function_Rami.h
@@ -1,1 +1,19 @@
+#ifndef FUNCTION_RAMI_H_INCLUDED
+#define FUNCTION_RAMI_H_INCLUDED
+typedef struct
+{
+int mois;
+int jour ;
+int annee ;
+}date ;
+typedef struct
+{
+int id;
+date date_debut_fin ;
+char municipalite[25];
+int nombres_conseilleurs ;
+}election ;
+
+int ajouter_election(election E , char elections [50]);
+#endif // FUNCTION_RAMI_H_INCLUDED
 
